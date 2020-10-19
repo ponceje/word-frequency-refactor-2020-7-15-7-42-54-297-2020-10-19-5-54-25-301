bc1d303e09c0a3ec4ac5cb826dd7167aeebcfc05
PONCEJE <PONCEJE@OOCL.COM>
2020-10-19 14:38:47 +0800

    [Word Frequency] [Jeyem] Refactor: Rename variable names to convenient name

--- src/test/java/WordInfoFrequencyGameTest.java
+++ src/test/java/WordInfoFrequencyGameTest.java
@@ -2,7 +2,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class WordFrequencyGameTest {
+public class WordInfoFrequencyGameTest {
 
     @Test
     public void should_get_the_1_when_input_the() throws Exception {
