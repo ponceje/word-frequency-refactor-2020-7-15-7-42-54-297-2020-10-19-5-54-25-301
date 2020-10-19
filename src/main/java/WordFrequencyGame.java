0d7acb3880c36aa023f46e9f43ab6ad02e74619f
PONCEJE <ponceje@oocl.com>
2020-10-19 15:06:41 +0800

    [Word Frequency] [Jeyem] Refactor: Rename w1 to firstWord and w2 to secondWord

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -33,7 +33,7 @@ public class WordFrequencyGame {
                 }
                 wordInfoList = distinctWordInfoList;
 
-                wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                wordInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
 
                 StringJoiner joiner = new StringJoiner("\n");
                 for (WordInfo w : wordInfoList) {
