d59a70fd6a90a2ddab097c7cc69cdc3aab7ed420
PONCEJE <ponceje@oocl.com>
2020-10-19 15:49:16 +0800

    [Word Frequency] [Jeyem] Refactor: extract logic and move to newly created getStringJoiner function

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -6,16 +6,18 @@ public class WordFrequencyGame {
     private static final String NEW_LINE = "\n";
 
     public String getResult(String inputStr){
-
         List<WordInfo> wordInfoList = calculateWordFrequency(inputStr) ;
         wordInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
+        return getStringJoiner(wordInfoList).toString();
+    }
+
+    private StringJoiner getStringJoiner(List<WordInfo> wordInfoList) {
         StringJoiner joiner = new StringJoiner(NEW_LINE);
         for (WordInfo wordInfo : wordInfoList) {
             String wordInfoLine = String.format("%s %d", wordInfo.getValue(), wordInfo.getWordCount());
             joiner.add(wordInfoLine);
         }
-        return joiner.toString();
-
+        return joiner;
     }
 
     private List<WordInfo> calculateWordFrequency(String sentence){
@@ -27,5 +29,6 @@ public class WordFrequencyGame {
         }
         return wordInfoList;
     }
+
 
 }
